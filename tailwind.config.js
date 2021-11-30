334c3a7ccbd76a7b8ce908f9a45e9d78eeab2039
Axhon <axhonruiz@gmail.com>
2021-11-29 17:11:17 -0800

    added tailwind css to the build step and dev step

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const fromRoot = p => path.join(__dirname, p)
+
+module.exports = {
+    mode: process.env.NODE_ENV ? 'jit' : undefined,
+    purge: {
+        mode: 'layers',
+        enabled: process.env.NODE_ENV === 'production',
+        content: [fromRoot('./app/**/*.+(js|ts|tsx|mdx|md)')],
+    },
+  darkMode: 'media',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
